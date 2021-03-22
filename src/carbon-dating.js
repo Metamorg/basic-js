005d8f51bb08eb331ec15a04498c67811c464972
Metamorg <egoronokasd@gmail.com>
2021-03-22 12:58:34 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,11 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!Number(sampleActivity) || typeof sampleActivity !== 'string' || sampleActivity <= 0 || sampleActivity > 15) {
+    return false;
+  } else {
+      return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/(0.693/HALF_LIFE_PERIOD));
+  }
+
 };
