c77277f2143f85b0fc9eba5349daa4fc7e0037e4
Metamorg <egoronokasd@gmail.com>
2021-03-22 14:02:29 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options;
+  str = String(str);
+  addition = String(addition);
+
+  let additionString = strRepeat(addition, additionSeparator, additionRepeatTimes);
+  return  strRepeat(str + additionString, separator, repeatTimes);  
 };
-  
+
+function strRepeat (string, sep, arr) {
+  return new Array(arr).fill(string).join(sep);
+}
